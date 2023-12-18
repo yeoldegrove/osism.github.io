9da8c1a3a4b17e2bc236de6029abf4070bc54197
Christian Berendt <berendt@osism.tech>
2023-12-18 11:32:45 +0100

    Change logo (#243)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,6 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'OSISM',
         logo: {
           alt: 'OSISM Logo',
           src: 'img/logo.svg',
