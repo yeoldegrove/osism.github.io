ab64a23525bcbf6a3f1ead10efb1070c00569fd1
Christian Berendt <berendt@osism.tech>
2023-12-18 18:22:10 +0100

    Add netdata to the home page (#250)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -66,6 +66,15 @@ const FeatureList = [
       </>
     ),
   },
+  {
+    title: 'AI & realtime insights with Netdata',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit.
+      </>
+    ),
+  },
 ];
 
 function Feature({Svg, title, description}) {
