b5aee4e63997fbe5710faf6b083171687b464110
Christian Berendt <berendt@osism.tech>
2024-04-21 13:04:23 +0200

    Replace titles in imported files (#433)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- get-external-files.py
+++ get-external-files.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env pyton3
 
+import re
+
 import requests
 
 FILES = [
@@ -8,12 +10,14 @@ FILES = [
         "repository": "SovereignCloudStack/docs",
         "target": "docs/guides/user-guide/openstack/security-groups.md",
         "header": "---\nsidebar_label: Security groups\n---\n\n",
+        "title": "How to configure and use security groups",
     },
     {
         "source": "https://raw.githubusercontent.com/SovereignCloudStack/openstack-health-monitor/main/docs/Debian12-Install.md",
         "repository": "SovereignCloudStack/openstack-health-monitor",
         "target": "docs/guides/other-guides/openstack-health-monitor.md",
         "header": "---\nsidebar_label: OpenStack Health Monitor\n---\n\n",
+        "title": "Setting up OpenStack health monitor on Debian",
     },
 ]
 
@@ -21,7 +25,13 @@ for file in FILES:
     r = requests.get(file["source"], allow_redirects=True)
     with open(file["target"], "wb") as fp:
         fp.write(str.encode(file["header"]))
-        fp.write(r.content)
+
+        if "title" in file:
+            text = re.sub("^# .*", f"# {file['title']}", r.text)
+            fp.write(str.encode(text))
+        else:
+            fp.write(r.rontent)
+
 
         footer = f"\n\n_The source of this document can be found in the [{file['repository']}]({file['source']}) repository._\n"
         fp.write(str.encode(footer))
